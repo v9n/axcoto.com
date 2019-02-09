de08d0b03161b7f3cccf2979e517b71813764ba8
Vinh <vinh@axcoto.com>
2019-02-09 10:30:44 -0800

    Update font, remove ga

--- themes/baja/static/js/main.js
+++ themes/baja/static/js/main.js
@@ -1,12 +1,3 @@
-var _gaq = _gaq || []
-_gaq.push(['_setAccount', 'UA-17051798-1'])
-_gaq.push(['_trackPageview'])
-
 (function() {
-    var ga = document.createElement('script')
-    ga.type = 'text/javascript'
-    ga.async = true
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
-    var s = document.getElementsByTagName('script')[0]
-    s.parentNode.insertBefore(ga, s)
+  console.log("If I'm a person that used ads block then I will not do that to the rest of world")
 })()
